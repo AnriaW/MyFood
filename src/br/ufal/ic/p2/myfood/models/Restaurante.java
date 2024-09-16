49a3002d57fc0959350f02cc48408476c1d1f9de
Antônio Wagner <138132417+AnriaW@users.noreply.github.com>
2024-09-15 23:55:15 -0300

    Update Restaurante.java

--- src/br/ufal/ic/p2/myfood/models/Restaurante.java
+++ src/br/ufal/ic/p2/myfood/models/Restaurante.java
@@ -3,7 +3,6 @@ package br.ufal.ic.p2.myfood.models;
 public class Restaurante extends Empresa {
     private String tipoCozinha;
 
-    // O construtor vazio é necessário para a serialização e desserialização
     public Restaurante() {
     }
 
@@ -12,7 +11,6 @@ public class Restaurante extends Empresa {
         this.tipoCozinha = tipoCozinha;
     }
 
-    // Getters e Setters
 
     public String getTipoCozinha() {
         return tipoCozinha;
@@ -22,11 +20,6 @@ public class Restaurante extends Empresa {
         this.tipoCozinha = tipoCozinha;
     }
 
-    /**
-     * Retorna o valor do atributo desejado da classe
-     * @param atributo o nome do atributo desejado
-     * @return Uma 'string' com o valor do atributo desejado
-     */
     @Override
     public String getAtributo(String atributo) {
         if (atributo.equals("tipoCozinha")) {
